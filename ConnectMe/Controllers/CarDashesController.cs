f77baca67e2fcf0d6c2cf4ddcb5a2439df64b9c4
Jon Reed <mr.is@outlook.com>
2015-05-04 16:26:58 -0400

    Added scaffolding controller and CarControllerHub

---
+++ ConnectMe/Controllers/CarDashesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ConnectMe.Models;
+
+namespace ConnectMe.Controllers
+{
+    public class CarDashesController : ApiController
+    {
+        private ConnectMeContext db = new ConnectMeContext();
+
+        // GET: api/CarDashes
+        public IQueryable<CarDash> GetCarDashes()
+        {
+            return db.CarDashes;
+        }
+
+        // GET: api/CarDashes/5
+        [ResponseType(typeof(CarDash))]
+        public IHttpActionResult GetCarDash(int id)
+        {
+            CarDash carDash = db.CarDashes.Find(id);
+            if (carDash == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(carDash);
+        }
+
+        // PUT: api/CarDashes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCarDash(int id, CarDash carDash)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != carDash.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(carDash).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarDashExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CarDashes
+        [ResponseType(typeof(CarDash))]
+        public IHttpActionResult PostCarDash(CarDash carDash)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CarDashes.Add(carDash);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = carDash.Id }, carDash);
+        }
+
+        // DELETE: api/CarDashes/5
+        [ResponseType(typeof(CarDash))]
+        public IHttpActionResult DeleteCarDash(int id)
+        {
+            CarDash carDash = db.CarDashes.Find(id);
+            if (carDash == null)
+            {
+                return NotFound();
+            }
+
+            db.CarDashes.Remove(carDash);
+            db.SaveChanges();
+
+            return Ok(carDash);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CarDashExists(int id)
+        {
+            return db.CarDashes.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ ConnectMe/Models/CarDash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConnectMe.Models
+{
+    public class CarDash
+    {
+        public int Id { get; set; }
+        public bool HeadLightStatus { get; set; }
+        public bool EngineStatus { get; set; }
+        public bool DoorLockStatus { get; set; }
+        public bool HornStatus { get; set; }
+    }
+}
